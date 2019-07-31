191890b4bce82e2cc9a2821e35b076f704221143
Aleksey Stepanov <a.i.stepanov@yandex.ru>
2019-07-31 20:44:50 +0300

    oops

--- client/client.go
+++ client/client.go
@@ -13,6 +13,22 @@ import (
 	"github.com/marcusolsson/tui-go"
 )
 
+type LoginHandler func(string)
+type SubmitMessageHandler func(string)
+
+type LoginView struct {
+	tui.Box
+	frame        *tui.Box
+	loginHandler LoginHandler
+}
+
+type ChatView struct {
+	tui.Box
+	frame    *tui.Box
+	history  *tui.Box
+	onSubmit SubmitMessageHandler
+}
+
 type Message struct {
 	Time	time.Time	`json:"time"`
 	Nick	string		`json:"nick"`
@@ -95,7 +111,6 @@ func setNickname(nickname string ) {
 	nick = &nickname
 }
 
-// connect connects to the local chat server at port <port>
 func connect() (*websocket.Conn, error) {
 	return websocket.Dial(fmt.Sprintf("ws://%s:%s", *host, *port), "", genMyOrigin())
 }
@@ -110,9 +125,94 @@ func genMyOrigin() string {
 	for _, a := range addrs {
 		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				return fmt.Sprintf("http://%v#%d", ipnet.IP.String(), rand.Int31())
+				return fmt.Sprintf("http://%v/#%d", ipnet.IP.String(), rand.Int31())
 			}
 		}
 	}
-	return fmt.Sprintf("http://0.0.0.0", rand.Int31())
+	return fmt.Sprintf("http://0.0.0.0/#", rand.Int31())
+}
+
+func NewLoginView() *LoginView {
+	user := tui.NewEntry()
+	user.SetFocused(true)
+	user.SetSizePolicy(tui.Maximum, tui.Maximum)
+
+	label := tui.NewLabel("Enter your nick: ")
+	user.SetSizePolicy(tui.Expanding, tui.Maximum)
+
+	userBox := tui.NewHBox(
+		label,
+		user,
+	)
+	userBox.SetBorder(true)
+	userBox.SetSizePolicy(tui.Expanding, tui.Maximum)
+
+	view := &LoginView{}
+	view.frame = tui.NewVBox(
+		tui.NewSpacer(),
+		tui.NewPadder(-4, 0, tui.NewPadder(4, 0, userBox)),
+		tui.NewSpacer(),
+	)
+	view.Append(view.frame)
+
+	user.OnSubmit(func(e *tui.Entry) {
+		if e.Text() != "" {
+			if view.loginHandler != nil {
+				view.loginHandler(e.Text())
+			}
+			e.SetText("")
+		}
+	})
+
+	return view
+}
+
+func (v *LoginView) OnLogin(handler LoginHandler) {
+	v.loginHandler = handler
+}
+
+func NewChatView() *ChatView {
+	view := &ChatView{}
+	view.history = tui.NewVBox()
+	historyScroll := tui.NewScrollArea(view.history)
+	historyScroll.SetAutoscrollToBottom(true)
+	historyBox := tui.NewVBox(historyScroll)
+	historyBox.SetBorder(true)
+
+	input := tui.NewEntry()
+	input.SetFocused(true)
+	input.SetSizePolicy(tui.Expanding, tui.Maximum)
+
+	input.OnSubmit(func(e *tui.Entry) {
+		if e.Text() != "" {
+			if view.onSubmit != nil {
+				view.onSubmit(e.Text())
+			}
+			e.SetText("")
+		}
+	})
+
+	inputBox := tui.NewHBox(input)
+	inputBox.SetBorder(true)
+	inputBox.SetSizePolicy(tui.Expanding, tui.Maximum)
+	view.frame = tui.NewVBox(
+		historyBox,
+		inputBox,
+	)
+	view.frame.SetBorder(false)
+	view.Append(view.frame)
+
+	return view
+}
+
+func (c *ChatView) OnSubmit(handler SubmitMessageHandler) {
+	c.onSubmit = handler
+}
+
+func (c *ChatView) AddMessage(time string, user string, msg string) {
+	c.history.Append(
+		tui.NewHBox(
+			tui.NewLabel(fmt.Sprintf("%v %v: %v", time, user, msg)),
+		),
+	)
 }
